bedf9365e80b162f2f0bb0c5633d5d8af5d3d1f0
w0jno <filip.wojno03@o2.pl>
2024-12-24 09:46:05 +0100

    initial commit for golang

---
+++ go-lox/Lox.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	bool hadError = false
+	if len(os.Args ) > 2{
+		fmt.Println("Usage: Lox [script]")
+		os.Exit(64)
+	} else if len(os.Args) == 2 {
+		runFile(os.Args[1])
+		if hadError {
+			os.Exit(65)
+		}
+	} else {
+		runPrompt()
+	}
+}
+
+func runFile(path string){
+	bytes, error := os.ReadFile(path)
+	if error == nil {
+		run(string(bytes))
+	}
+}
+
+func runPrompt(){
+	for {
+		fmt.Print("> ")
+		var input string
+		fmt.Scanln(&input)
+		run(input)
+		hadError = false
+	}
+}
+
+func run(source string){
+	scanner := Scanner(source)
+	tokens := scanner.scanTokens()
+
+	for _, token : range tokens {
+		fmt.Println(token)
+	}
+}
+
+func error(int line, string message){
+	report(line, "", message)
+}
+
+func report(int line, string where, string message){
+	fmt.Printf("[line %d] Error %s: %s\n", line, where, message)
+}
